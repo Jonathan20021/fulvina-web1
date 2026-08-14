8311fb0d5cf782260f7b4318da4efdae7c59a58a
Jonathan Sandoval <jonathansandovalferreira@gmail.com>
2026-08-14 15:44:53 -0400

    3.24

--- includes/analytics.php
+++ includes/analytics.php
@@ -39,6 +39,7 @@ function quote_categories(): array
         'Diseño hospitalario'         => ['ruler', '#1fa6d8'],
         'Instalación y certificación' => ['wrench', '#9c7d34'],
         'Soporte y mantenimiento'     => ['life-buoy', '#0666b3'],
+        'Equipos industriales'        => ['factory', '#475569'],
     ];
 }
 
@@ -297,6 +298,7 @@ function analytics_revenue_by_line(?array $period = null): array
         $demo = [
             ['Equipos médicos', 539000], ['Gases medicinales', 295000], ['Diseño hospitalario', 205000],
             ['Instalación y certificación', 154000], ['Soporte y mantenimiento', 91500],
+            ['Equipos industriales', 128000],
         ];
         if (!analytics_live()) {
             $total = array_sum(array_column($demo, 1)) ?: 1;
